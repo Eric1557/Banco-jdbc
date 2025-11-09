e61c9400f55a48f284420ab6fad2afa7bdf61677
Eric1557 <ericbbarbosa2019@gmail.com>
2025-11-09 15:32:30 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department department = new Department(1,"Books");
-        System.out.println(department);
 		
+		Seller seller = new Seller(1,"Eric Bastos Barbosa","eric@gmail.com",new Date(),2.000,department);
+		
+        System.out.println(seller);
 		
 	}
 
