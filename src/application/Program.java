95b1f66dbbe7db75b2a740a14073a18da3e634e1
Eric1557 <ericbbarbosa2019@gmail.com>
2025-11-15 10:09:56 -0300

    DaoFactory and SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,9 +13,13 @@ public class Program {
 		
 		Department department = new Department(1,"Books");
 		
-		Seller seller = new Seller(1,"Eric Bastos Barbosa","eric@gmail.com",new Date(),2.000,department);
+		Seller seller = new Seller(1," Eric Bastos Barbosa ", " eric@gmail.com ",new Date(),2.000,department);
 		
-        System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerJDBC();
+	
+	   System.out.println(sellerDao);
+	
+       System.out.println(seller);
 		
 	}
 
