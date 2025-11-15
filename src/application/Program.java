3cae92f3c6cd02c7908e1579ad6bdcc367dbedc0
Eric1557 <ericbbarbosa2019@gmail.com>
2025-11-15 12:25:51 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1,"Books");
 		
-		Seller seller = new Seller(1," Eric Bastos Barbosa ", " eric@gmail.com ",new Date(),2.000,department);
-		
-		SellerDao sellerDao = DaoFactory.createSellerJDBC();
+	  SellerDao sellerDao = DaoFactory.createSellerJDBC();
+	  System.out.println("=== teste1 :seller FindByld ===");
+	  Seller seller = sellerDao.FindByld(3);
+	  System.out.println(seller);
 	
-	   System.out.println(sellerDao);
-	
-       System.out.println(seller);
+      
 		
 	}
 
