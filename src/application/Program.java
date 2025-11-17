a43fd5b2f1792b08118501923d92ca484590c41e
Eric1557 <ericbbarbosa2019@gmail.com>
2025-11-17 12:07:40 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,13 +13,15 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+
 		SellerDao sellerDao = DaoFactory.createSellerJDBC();
-		System.out.println("\n === teste1 :seller FindByld() === \n");
+		System.out.println("\n === TESTE 1 :seller FindByld() === \n");
 
 		Seller seller = sellerDao.FindByld(3);
 		System.out.println(seller);
 
-		System.out.println("\n === teste2 :seller FindByldDepartment() === \n");
+		System.out.println("\n === TESTE 2 :seller FindByldDepartment() === \n");
 
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.FindByldDepartment(department);
@@ -27,26 +30,31 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		System.out.println("\n === teste3 :seller FindALL() === \n");
+		System.out.println("\n === TESTE 3 :seller FindALL() === \n");
 		list = sellerDao.findAll();
 
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		System.out.println("\n === teste4 :seller Insert() === \n");
+		System.out.println("\n === TESTE 4 :seller Insert() === \n");
 		Seller newSeller = new Seller(null, "Eric ", "eric@gmail.com ", new Date(), 2500.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted ! new id = " + newSeller.getId());
-		
-		System.out.println("\n === teste5 :seller Update() === \n");
-		seller = sellerDao.FindByld(1);
+
+		System.out.println("\n === TEST 5 :seller Update() === \n");
+		seller = sellerDao.FindByld(2);
 		seller.setName("Eric Bastos");
 		sellerDao.update(seller);
 		System.out.println("Update complete !");
-		
+
+		System.out.println("\n === TEST 6 :seller Update() === \n");
+		System.out.println("enter Id for deleted test : ");
+		int id = sc.nextInt();
+		sellerDao.deleteByld(id);
+		System.out.println("Deleted Completed");
+
+		sc.close();
+
 	}
 
 }
-
-
-
--- src/model/dao/Impl/SellerDaoJDBC.java
+++ src/model/dao/Impl/SellerDaoJDBC.java
@@ -87,7 +87,23 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteByld(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected == 0) {
+				throw new DbException("id Invalid ! try again ");
+			}
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 
 	}
 
